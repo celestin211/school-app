ee036c6b21b9a1f3a9f136fa293e4ae0775165f8
Sonia CHOYTUN <celestin.mombela@yahoo.fr>
2023-11-14 19:45:01 +0100

    css fix

--- assets/js/navbar-change.js
+++ assets/js/navbar-change.js
@@ -1,20 +1,23 @@
-(function() {
-	function toggleNav() {
-// Define targets
-		var target = document.querySelector('.main');
-		var button = document.querySelector('.burger-button');
-		var navigation = document.querySelector('.navigation');
+var toggles = document.querySelectorAll(".c-hamburger");
 
-// click-touch event
-		if ( button ) {
-			button.addEventListener('click',
-				function (e) {
-					target.classList.toggle('is-opened');
-					navigation.focus();
-					e.preventDefault();
-				}, false );
+for (var i = toggles.length - 1; i >= 0; i--) {
+	var toggle = toggles[i];
+	toggleHandler(toggle);
+};
+
+function toggleHandler(toggle) {
+	toggle.addEventListener("click", function(e) {
+		e.preventDefault();
+		if (this.classList.contains("is-active") === true) {
+			this.classList.remove("is-active");
+			$('.open').removeClass('oppenned');
+		} else {
+			this.classList.add("is-active");
+			$(".open").addClass('oppenned');
 		}
-	} // end toggleNav()
-	
-	toggleNav();
-}());
+	});
+}
+$(".sub-menu li a").click(function(event) {
+	$(".open").removeClass('oppenned');
+	$(".c-hamburger").removeClass('is-active');
+});
